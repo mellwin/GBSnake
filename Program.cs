e161c9e14deded167f7788d6dccce9130ed52498
mellwin <oaparamonov@mail.ru>
2023-11-08 23:42:35 +0300

    HorizontalLine

--- Program.cs
+++ Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApp
 
             Point p2 = new Point(4,5,'#');
             p2.Draw();
+
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
         }
     }
 }
