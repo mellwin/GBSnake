80e3069b1d214910975b69810fc9986864a9be1a
mellwin <oaparamonov@mail.ru>
2023-11-09 00:31:41 +0300

    Snake and abstraction

--- Point.cs
+++ Point.cs
@@ -19,10 +19,33 @@ namespace ConsoleApp
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT) x += offset;
+
+            if (direction == Direction.LEFT) x -= offset;
+
+            if (direction == Direction.UP) y += offset;
+
+            if (direction == Direction.DOWN) y += offset;
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -21,11 +21,13 @@ namespace ConsoleApp
 
 
 
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Point p = new Point(4, 5, '*');
+            p.Draw();
 
-            Point p2 = new Point(4,5,'#');
-            p2.Draw();
+
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
 
             Console.ReadKey();
         }
